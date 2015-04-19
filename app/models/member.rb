7df7a0a4b98f891e106bf742f24fc09821ba5985
David <david.norrestam@hotmail.com>
2015-04-19 19:10:24 +0200

    asd

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,29 +1,59 @@
+require 'csv'
 class Member < ActiveRecord::Base
 	has_many :contacteds, dependent: :destroy
 	has_many :jobs, dependent: :destroy
-
 	def self.import(file)
-		CSV.foreach(file) do |row|
-	  # use row here...
-	end
+		# Read the imported csv into variable
+		spreadsheet = CSV.read(file.path)
 
-	  spreadsheet = open_spreadsheet(file)
-	  header = spreadsheet.row(1)
-	  (2..spreadsheet.last_row).each do |i|
-	    row = Hash[[header, spreadsheet.row(i)].transpose]
-	    product = find_by_id(row["id"]) || new
-	    product.attributes = row.to_hash.slice(*accessible_attributes)
-	    product.save!
+		index = 0
+
+		# Iterate through all rows
+	  spreadsheet.each do |row|
+	  	if index == 0 
+	  		index += 1
+		  else
+		  	# Create a new member
+		  	new_member = Member.new
+
+			  #x, datum inskriven?
+			  new_member.dateAdded = row[0]
+
+			  #Namn
+			  first_name = row[1]
+			  
+			  #Efternamn
+			  last_name = row[2]
+
+			  new_member.name = first_name + " " + last_name # is this right?
+
+			  #Personnummer,
+			  new_member.personId = row[3]
+
+			  #Telefonnummer,
+			  new_member.mobile = row[4]
+
+			  #Vill Jobba,
+			  
+			  #Email,
+			  new_member.email = row[6]
+			  
+			  #Termin,
+			  new_member.term = row[7]
+
+			  #Senast kontaktad,
+			  #7
+
+			  #Kommentarer,
+				#8
+
+			  #Jobbarkort
+
+			  # Save to database
+			  new_member.save
+
+			  index += 1
+			end
 	  end
 	end
-
-	def self.open_spreadsheet(file)
-	  case File.extname(file.original_filename)
-	  when ".csv" then Csv.new(file.path, nil, :ignore)
-	  when ".xls" then Excel.new(file.path, nil, :ignore)
-	  when ".xlsx" then Excelx.new(file.path, nil, :ignore)
-	  else raise "Unknown file type: #{file.original_filename}"
-	  end
-	end
-
 end

2e0def852043583503543e7b5de9dfc2cdc021a2
pbl <philip.burenstam.linder@gmail.com>
2015-05-02 17:04:42 +0200

    asd

---
+++ db/migrate/20150502140452_create_jobs.rb
@@ -0,0 +1,14 @@
+class CreateJobs < ActiveRecord::Migration
+  def change
+    create_table :jobs do |t|
+    	t.date :date
+    	t.string :activity
+    	t.text :comment
+
+    	t.references :member, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :jobs, :members
+  end
+end

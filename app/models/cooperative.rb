8385a301510c802012913fbf6fbe197031dfa4e6
pbl <philip.burenstam.linder@gmail.com>
2015-05-03 01:43:38 +0200

    solved user assocation to cooperative

--- app/models/cooperative.rb
+++ app/models/cooperative.rb
@@ -1,8 +1,6 @@
 class Cooperative < ActiveRecord::Base
 	has_many :users
 	has_many :members
-	# has_many :jobs, through: :members
-	# has_many :contacteds, through: :members
 
 	validates_presence_of :name
 

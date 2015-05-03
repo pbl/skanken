2c22c59540bb18bdcaeb3279fadccd54a38ae1f2
Erik <tpi11ehe@student.lu.se>
2015-05-03 19:02:14 +0200

    Pressing Import without choosing a file will no longer cause a chrash.

--- app/controllers/cooperatives_controller.rb
+++ app/controllers/cooperatives_controller.rb
@@ -33,7 +33,7 @@ class CooperativesController < ApplicationController
   end
 
   def import
-    Member.import(params[:file], current_user.cooperative_id)
+    Member.import(params[:file], current_user.cooperative_id) unless params[:file].nil?
     redirect_to cooperative_admin_path
   end
 

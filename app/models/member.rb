45d613918c4619143d0e4bb2b1747d3cc6b62a6f
Erik <tpi11ehe@student.lu.se>
2015-05-03 20:25:19 +0200

    Import will now reformat phone numbers.

--- app/models/member.rb
+++ app/models/member.rb
@@ -10,7 +10,7 @@ class Member < ActiveRecord::Base
   ACTIVITES = {
     svartklubben: "Svartklubben",
     heartland:    "Heartland",
-    foxen:         "Foxen"
+    foxen:         "Foxen",
   }
 
   def self.activities
@@ -18,13 +18,35 @@ class Member < ActiveRecord::Base
     activites_map.to_a.map! { |arr| [arr.first.to_s.humanize, arr.last] }
   end
 
+
   def self.import(file, cooperative_id)
-    workerList = CSV.read(file.path, headers:true)
-    @cooperative = Cooperative.find(cooperative_id)
+    workerList = CSV.read(file.path, headers: true)
     workerList.each do |row|
-      @member = @cooperative.members.new(:dateAdded=>row['x'].to_s, :name=> (row['Namn'].to_s + " " + row['Efternamn'].to_s) ,:mobile=>row['Telefonnummer'].to_s, :email=>row['Email'].to_s, :personId=>row['Personnummer'].to_s, :activities=>row['Vill Jobba'].to_s)
+
+      @cooperative = Cooperative.find(cooperative_id)
+      @member = @cooperative.members.new(:dateAdded => row['x'].to_s, :name => (row['Namn'].to_s + " " + row['Efternamn'].to_s), :mobile => validate_number(row['Telefonnummer']), :email => row['Email'].to_s, :personId => row['Personnummer'].to_s, :activities => row['Vill Jobba'].to_s)
+
       @member.save
+      begin
+        @contacted = @member.contacteds.new(:date => Date.parse(row['Senast kontaktad'].to_s), :activity => 'Unknown', :comment => row['Kommentarer'].to_s)
+        @contacted.save
+      rescue
+      end
+
     end
+  end
+
+  private
+  def self.validate_number(number)
+    @number=number.to_s.strip.delete("-")
+    begin
+      @number=@number.strip
+      @number.prepend("0") if @number.start_with?("7")
+      @number=@number.sub("+46", "0") if @number.start_with?("+46")
+      @number=@number.insert(3, "-")
+    rescue
+    end
+    return @number
   end
 
 end

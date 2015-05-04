3145136fd4776cdd1c4315e12ca4938d62026eb6
pbl <philip.burenstam.linder@gmail.com>
2015-05-05 00:53:19 +0200

    removed old which was commented away

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,50 +2,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-	# Handle the case where we don't have a current_user i.e. the user is a guest
-	user ||= User.new
+		# Handle the case where we don't have a current_user i.e. the user is a guest
+		user ||= User.new
 
-	# Define a few sample abilities
-	# cannot :manage , Comment
-	# can    :read   , Tag , released: true
-# asd
-	if user.is? :admin
-		can :manage, :all
-	elsif user.is? :cooperative_admin
-		can :manage, :all, :active => true, :cooperative_id => user.cooperative_id
-	elsif user.is? :foreman
-		can :manage, Member, :active => true, :cooperative_id => user.cooperative_id
-		can :manage, Job, :active => true, :cooperative_id => user.cooperative_id
-		can :manage, Contacted, :active => true, :cooperative_id => user.cooperative_id
-	else
-		can :manage, Info
-	end
-
-	# Define abilities for the passed in user here. For example:
-	#
-	#   user ||= User.new # guest user (not logged in)
-	#   if user.admin?
-	#     can :manage, :all
-	#   else
-	#     can :read, :all
-	#   end
-	#
-	# The first argument to `can` is the action you are giving the user
-	# permission to do.
-	# If you pass :manage it will apply to every action. Other common actions
-	# here are :read, :create, :update and :destroy.
-	#
-	# The second argument is the resource the user can perform the action on.
-	# If you pass :all it will apply to every resource. Otherwise pass a Ruby
-	# class of the resource.
-	#
-	# The third argument is an optional hash of conditions to further filter the
-	# objects.
-	# For example, here the user can only update published articles.
-	#
-	#   can :update, Article, :published => true
-	#
-	# See the wiki for details:
-	# https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+		if user.is? :admin
+			can :manage, :all
+		elsif user.is? :cooperative_admin
+			can :manage, :all, :active => true, :cooperative_id => user.cooperative_id
+		elsif user.is? :foreman
+			can :manage, Member, :active => true, :cooperative_id => user.cooperative_id
+			can :manage, Job, :active => true, :cooperative_id => user.cooperative_id
+			can :manage, Contacted, :active => true, :cooperative_id => user.cooperative_id
+		else
+			can :manage, Info
+		end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,37 +16,4 @@ class User < ActiveRecord::Base
   def is?( requested_role )
     self.role == requested_role.to_s
   end
-
-  # PUBLIC_REG_ROLES = [:tenant, :cooperative_admin]
-
-  # def self.is? user_role
-  #   if user_role == :admin
-  #     return self.user_role == 1337
-  #   elsif user_role == :cooperative_admin
-  #     return self.user_role == 2
-  #   elsif user_role == :foreman
-  #     return self.user_role == 1
-  #   else
-  #     return self.user_role == -1
-  #   end
-  # end
-
-  # def self.role_name role_i
-  #   name = ''
-  #   if role_i == 1
-  #     name = 'Foreman'
-  #   elsif role_i == 2
-  #     name = 'Cooperative admin'
-  #   elsif role_i == -1
-  #     name = 'No role'
-  #   end
-  #   return name      
-  # end
-
-  # def self.zip_user_roles(only = :all)
-  #   user_roles = User::ROLES
-  #   user_roles = user_roles.reject { |e| e == :no_role ||  e == :admin } if only == :registration
-  #   user_roles.to_a.map! { |arr| [arr.first.to_s.humanize, arr.last] }
-  # end
-
 end

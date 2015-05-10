075546a81917b3d1e23c55eee43c9188e77dc991
pbl <philip.burenstam.linder@gmail.com>
2015-05-11 01:24:24 +0200

    made worker list table mobile responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,57 +17,58 @@
 
 $( document ).ready(function() {
   // Jquery datatable code
-$('#worker_table').DataTable( {
-  initComplete: function () {
-    this.api().columns().every( function () {
-      var column = this;
-
-      // only create a filter for the column activities
-      if($(column.footer())[0].textContent === "Activities"){
-        var select = $('<select><option value=""></option></select>')
-          .appendTo( $(column.header()).empty() )
-          .on( 'change', function () {
-            var val = $.fn.dataTable.util.escapeRegex(
-              $(this).val()
-            );
-            // the standard filter function
-            // column
-            //   .search( val ? '^'+val+'$' : '', true, false )
-            //   .draw();
-
-            // adjusted filter function to fit worker list purpose
-            column
-            .search( val, true, false )
-            .draw();
-          } );
-
-        var unique_activities = []
-        
-        column.data().unique().sort().each( function ( d, j ) {
-          var activities = d.split("\, ")
-          for (var i = 0; i < activities.length; i++) {
-            // see if the activity option is already added
-            if(unique_activities.indexOf(activities[i]) === -1 ){
-              unique_activities.push(activities[i])
-            }
-          }  
-        } );
-        for (var i = 0; i < unique_activities.length; i++) {
-          select.append( '<option value="'+unique_activities[i]+'">'+unique_activities[i]+'</option>' )
-        }
-      }
-      // clear away the datatable footer
-      $(column.footer()).empty()
-    } );
-  }
-} );
+  $('#worker_table').DataTable( {
+    stateSave: true,
+    initComplete: function () {
+      jquery_data_table(this)
+    }
+  } );
 
 });
+// 
+function jquery_data_table(that){
+  var self = that;
+  self.api().columns().every( function () {
+    var column = this;
+    // only create a filter for the column activities
+    if($(column.footer())[0].textContent === "Activities"){
+      var select = $('<select><option value=""></option></select>')
+        .appendTo( $($("#activities-filter-header")).empty() )
+        .on( 'change', function () {
+          var val = $.fn.dataTable.util.escapeRegex(
+            $(this).val()
+          );
+          // the standard filter function
+          // column
+          //   .search( val ? '^'+val+'$' : '', true, false )
+          //   .draw();
+
+          // adjusted filter function to fit worker list purpose
+          column
+          .search( val, true, false )
+          .draw();
+        } );
+
+      var unique_activities = []
+      
+      column.data().unique().sort().each( function ( d, j ) {
+        var activities = d.split("\, ")
+        for (var i = 0; i < activities.length; i++) {
+          // see if the activity option is already added
+          if(unique_activities.indexOf(activities[i]) === -1 ){
+            unique_activities.push(activities[i])
+          }
+        }  
+      } );
+      for (var i = 0; i < unique_activities.length; i++) {
+        select.append( '<option value="'+unique_activities[i]+'">'+unique_activities[i]+'</option>' )
+      }
+      
+    } 
+    $(column.footer()).empty()
+  } );
+}
 
 function formActionValue(id, type){
   $('#modal_form_' + type).find('form').attr('action', 'members/' + id + '/' + type);
-}
-
-function show_worker(show_url){
-  window.open(show_url, "_self");
 }

553dd643056bf1d6d7b3daa2b6cefdc8fe07e213
pbl <philip.burenstam.linder@gmail.com>
2015-05-14 20:26:59 +0200

    fixed jquery data table bug which was still there

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,55 +15,6 @@
 //= require turbolinks
 //= require_tree .
 
-$( document ).ready(function() {
-});
-
-function jquery_data_table(that){
-  var self = that;
-  self.api().columns().every( function () {
-    var column = this;
-    // only create a filter for the column activities
-    if($(column.footer())[0].id === "activities-footer"){
-      var select = $('<select><option value="">Work with filter</option></select>')
-        .css("margin-left", "10px")
-        .css("float", "right")
-        // .appendTo( $("#activities-filter-header").empty() )
-        .insertBefore( $("#worker_table_filter") )
-        .on( 'change', function () {
-          var val = $.fn.dataTable.util.escapeRegex(
-            $(this).val()
-          );
-          // the standard filter function
-          // column
-          //   .search( val ? '^'+val+'$' : '', true, false )
-          //   .draw();
-
-          // adjusted filter function to fit worker list purpose
-          column
-          .search( val, true, false )
-          .draw();
-        } );
-
-      var unique_activities = []
-      
-      column.data().unique().sort().each( function ( d, j ) {
-        var activities = d.split("\, ")
-        for (var i = 0; i < activities.length; i++) {
-          // see if the activity option is already added
-          if(unique_activities.indexOf(activities[i]) === -1 ){
-            unique_activities.push(activities[i])
-          }
-        }  
-      } );
-      for (var i = 0; i < unique_activities.length; i++) {
-        select.append( '<option value="'+unique_activities[i]+'">'+unique_activities[i]+'</option>' )
-      }
-      
-    } 
-    $(column.footer()).empty()
-  } );
-}
-
 function formActionValue(id, type){
   $('#modal_form_' + type).find('form').attr('action', 'members/' + id + '/' + type);
 }

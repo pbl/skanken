5bde5334cec2510151d12b37a0743c424f70f295
pbl <philip.burenstam.linder@gmail.com>
2015-05-19 01:03:54 +0200

    wrote first controller test

--- test/controllers/info_controller_test.rb
+++ test/controllers/info_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class InfoControllerTest < ActionController::TestCase
+
   test "should get index" do
-    get :index
+  	sign_in users :default_user
+    get :about
     assert_response :success
+    assert_template :about
+	  assert_template layout: "layouts/application"
+    sign_out :joe
   end
 
 end
--- test/controllers/members_controller_test.rb
+++ test/controllers/members_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class MembersControllerTest < ActionController::TestCase
- test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:members)
-  end
+ 
+ # test "should get index" do
+ #    get :index
+ #    assert_response :success
+ #    assert_not_nil assigns(:members)
+ #  end
 end

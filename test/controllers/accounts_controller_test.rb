4961761e2f239759a05dd72ddb440297ca7f6ccb
pbl <philip.burenstam.linder@gmail.com>
2015-05-20 09:50:46 +0200

    checkpoint

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class AccountsControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
+  test "should get index for cooperative_admin" do
+  	user = users(:cooperative_admin_user)
+  	sign_in user
+  	cooperative = cooperatives(:cooperative)
+  	cooperative.users << user
+    get :index, :cooperative_id => cooperative.id
+    assert_response :success
+    assert_template :index
+	  assert_template layout: "layouts/application"
+    sign_out :cooperative_admin
+  end
 
 end

51f0b137637610235ec2c1f9d2762c8fdea52f24
pbl <philip.burenstam.linder@gmail.com>
2015-08-01 17:18:06 +0200

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from CanCan::AccessDenied do | exception |
-    redirect_to root_url, alert: exception.message
-  end
+  # rescue_from CanCan::AccessDenied do | exception |
+  #   redirect_to root_url, alert: exception.message
+  # end
 
-  def ensure_cooperative_created
-    return true unless (current_user.cooperative_id.nil?)
-    redirect_to new_cooperative_path
+  def ensure_access_members
+    return true unless (!current_user.cooperative_id.to_s.eql?(params[:cooperative_id]))
+    render nothing: true, status: 401
   end
 
   def user_start_page
@@ -18,10 +18,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
-    def date_today
-      time = Time.new
-      unformatTime = Time.local(time.year, time.month, time.day).to_s
-      formattedTime = unformatTime.gsub(/\s+/m, ' ').strip.split(" ")[0]
-      return formattedTime
-    end
+  def date_today
+    time = Time.new
+    unformatTime = Time.local(time.year, time.month, time.day).to_s
+    formattedTime = unformatTime.gsub(/\s+/m, ' ').strip.split(" ")[0]
+    return formattedTime
+  end
 end

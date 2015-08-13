a2bbf404a4f2fe11f4b5c1790006e4c04b021eee
pbl <philip.burenstam.linder@gmail.com>
2015-08-13 14:20:44 +0200

    stable import done

--- app/controllers/cooperatives_controller.rb
+++ app/controllers/cooperatives_controller.rb
@@ -17,16 +17,18 @@ class CooperativesController < ApplicationController
 
 	def import
 		if !params[:file].nil? && params[:file].original_filename.end_with?(".csv")
-			Member.import(params[:file], current_user.cooperative_id)
-			flash[:success] = "File uploaded. Helge vare gösta"
+			import = Import.new(params[:file], current_user.cooperative)
+			import.import
+			# flash[:success] = "File uploaded. Helge vare gösta"
 			redirect_to table_table_path
 		else
-			flash[:danger] = "No file or wrong file format. Helge vare gösta"
+			# flash[:danger] = "No file or wrong file format. Helge vare gösta"
 			redirect_to cooperative_admin_path
 		end
 	end
 
 	private
+
 	def cooperative_params
 		params.require(:cooperative).permit(:name)
 	end

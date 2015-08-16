59c4d58257e8d86cdd2dd620fabed93b47742ba1
pbl <philip.burenstam.linder@gmail.com>
2015-08-16 19:44:43 +0200

    asd

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,6 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+# for discovering mobile devise
+# Mime::Type.register_alias "text/html", :mobile

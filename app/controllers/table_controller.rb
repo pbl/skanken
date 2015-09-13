e78a46bf9223252abd9554a398a0cf9e69b08d60
pbl <philip.burenstam.linder@gmail.com>
2015-09-13 15:10:52 +0200

    asd

--- app/controllers/table_controller.rb
+++ app/controllers/table_controller.rb
@@ -1,21 +1,61 @@
+# class TableController < ApplicationController
+#   prepend_before_filter :authenticate_user!
+#   before_filter :set_activity_from_param, only: [:create]
+#   before_filter :user_activity, only: [:table]
+#   before_filter :set_cooperative, only: [:choose, :table]
+
+#   def choose
+#     @activities = @cooperative.activities.reject {|activity| !activity.activated}
+#     current_user.activities.delete_all
+#   end
+
+#   def create
+#     current_user.activities << @activity
+#     redirect_to table_table_path
+#   end
+
+#   def table
+#     page = params[:page] || 1
+#     @search_form    = SearchPresenter.new(params)
+#     query = @search_form.query || ''
+#     search = Search.new(@cooperative, @activity)
+#     @members = search.search(page: page, query: query)
+#   end
+
+#   private
+
+#   def user_activity
+#     @activity = current_user.activities.first
+#     return true unless @activity.nil?
+#     redirect_to table_choose_path
+#   end
+
+#   def set_activity_from_param
+#     @activity = current_user.cooperative.activities.find_by_id(params[:activity_id])
+#     record_exists?(@activity)
+#   end
+# end
 class TableController < ApplicationController
   prepend_before_filter :authenticate_user!
-  before_filter :set_activity_from_param, only: [:create]
-  before_filter :user_activity, only: [:table]
-  before_filter :set_cooperative, only: [:choose, :table]
+  before_filter :activity_param, only: [:create]
+  before_filter :set_activity, only: [:table]
+  before_action :set_cooperative, only: [:choose, :table]
 
   def choose
-    @activities = @cooperative.activities.reject {|activity| !activity.activated}
-    current_user.activities.delete_all
+    @activities = @cooperative.activities
+    @activated = @activities.reject {|activity| !activity.activated}
+    @user = current_user
   end
 
   def create
-    current_user.activities << @activity
+    activity = Activity.find(@activity_id)
+    current_user.activities.delete_all
+    current_user.activities << activity
     redirect_to table_table_path
   end
 
   def table
-    page = params[:page] || 1
+    page = params[:page].nil? ? 1 : params[:page]
     @search_form    = SearchPresenter.new(params)
     query = @search_form.query || ''
     search = Search.new(@cooperative, @activity)
@@ -24,14 +64,15 @@ class TableController < ApplicationController
 
   private
 
-  def user_activity
+  def set_activity
     @activity = current_user.activities.first
     return true unless @activity.nil?
     redirect_to table_choose_path
   end
 
-  def set_activity_from_param
-    @activity = current_user.cooperative.activities.find_by_id(params[:activity_id])
-    record_exists?(@activity)
+  def activity_param
+    @activity_id = params[:choose].try(:[], :activity_id).nil? ? 0 : params[:choose].try(:[], :activity_id).to_i
+    return true unless @activity_id == 0
+    render nothing: true, status: 401
   end
 end

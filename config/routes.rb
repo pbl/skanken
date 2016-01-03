b045d3d5cecad318f7acf4ff1e9d175f5f54dc1b
pbl <philip.burenstam.linder@gmail.com>
2016-01-03 04:55:19 +0100

    changed start page to devise log in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,40 @@
 Rails.application.routes.draw do
-
-
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
 
+  devise_scope :user do
+    authenticated :user do
+      root 'table#all', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   resources :activities, except: [:index, :show] do
     get 'deactivate', to: 'activities#deactivate'
   end
-  # resources :users
 
   resources :cooperatives, only: [:edit, :update] do
     resources :accounts
-
     post 'import', to: 'cooperatives#import'
     get 'accounts', to: 'cooperatives#accounts'
-
   end
+
   resources :members, except: [:index] do
     resources :jobs, only: [:create, :destroy]
     resources :contacteds, only: [:destroy]
     get 'add', to: 'contacteds#add'
   end
 
-
   namespace :table do
     get 'choose'
     get 'create'
     get 'all'
   end
-  # get 'choose', to: 'table#choose'
-  # get 'create', to: 'table#create'
 
   get 'history', to: 'history#show'
-
-  # get 'pages/about'
-  root 'pages#start'
-
 end

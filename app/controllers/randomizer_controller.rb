ff7cbadd910f8726ee792c019a457e81d336374e
pbl <philip.burenstam.linder@gmail.com>
2016-01-06 14:26:56 +0100

    can now randoize a volenteer

--- app/controllers/randomizer_controller.rb
+++ app/controllers/randomizer_controller.rb
@@ -1,7 +1,26 @@
 class RandomizerController < ApplicationController
+  prepend_before_filter :authenticate_user!
+  before_filter :set_activity_from_param, only: [:show]
+  before_filter :set_cooperative
+
   def category
+    @activities = @cooperative.activities.reject {|activity| !activity.activated}
   end
 
   def show
+    i = 1
+    while @member.nil?
+      i += 1
+      @member = @activity.members.where('nbr_contacteds < ?', i).sample
+    end
+    @member.contacteds.create(activity: @activity.name)
+    update_member
+  end
+
+  private
+
+  def set_activity_from_param
+    @activity = current_user.cooperative.activities.find_by_id(params[:activity_id])
+    record_exists?(@activity)
   end
 end

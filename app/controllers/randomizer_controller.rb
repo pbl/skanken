911a5dfdc4c6cef52f5c6b8cb8865b73946e2a30
pbl <philip.burenstam.linder@gmail.com>
2016-01-20 14:01:58 +0100

    fixed bug if no member is connected to an activity

--- app/controllers/randomizer_controller.rb
+++ app/controllers/randomizer_controller.rb
@@ -10,12 +10,17 @@ class RandomizerController < ApplicationController
   def show
     @user_member = UserMember.new
     i = 1
-    while @member.nil?
+    while @member.nil? && i < 100
       i += 1
       @member = @activity.members.where('nbr_contacteds < ?', i).sample
     end
-    @member.contacteds.create(activity: @activity.name)
-    update_member
+    if @member.nil?
+      flash[:warning] = t('randomizer.no_member_in_category', activity: @activity.name)
+      redirect_to randomizer_category_path
+    else
+      @member.contacteds.create(activity: @activity.name)
+      update_member
+    end
   end
 
   private
